20c9a9c4423da285d02d4f2e46c019d4096d429b
Ayushi Gupta <107073709+ayushigupta2409@users.noreply.github.com>
2023-02-21 23:38:33 +0530

    Add files via upload

---
+++ bookListComponent.js
@@ -0,0 +1,31 @@
+import { LightningElement } from 'lwc'
+const BOOK_URL = 'https://www.googleapis.com/books/v1/volumes?q='
+
+export default class BookListComponent extends LightningElement {
+    query='life'
+    books
+    timer
+    connectedCallback(){
+        this.fetchBookData()
+    }//in order to show data on page load, we use connectedcallback lifecyclehook
+    fetchBookData(){
+        fetch(BOOK_URL+this.query)//fetch api in js provides interface for fetching resources and this fetch method return a promise
+        //to which we apply then and catch
+        .then(response=>response.json())/*response is not in json, so we convert it into json. Also, in js, if then()
+        returns something, it is another promise altogether, so we put then and catch over that
+        */
+        .then(data=>{
+            console.log(data)
+            this.books=data
+        })
+        .catch(error=>console.error(error))
+    }
+    fetchBooksHandler(event){
+        this.query = event.target.value
+        window.clearTimeout(this.timer)//debouncing technique for searching
+        this.timer=setTimeout(()=>{
+            this.fetchBookData()
+        },1000)//we use setTimeOut so that fetchBookData runs after some time, otherwise it would keep calling
+    }//performance efficient techninque for searching
+
+}
